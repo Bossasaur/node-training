48a580f9cf93ed779f67b35c4908cc5279de849f
Bossasaur <mikeksoior@gmail.com>
2019-06-12 20:20:53 -0400

    Create hello.js

---
+++ Zenva Training/hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+function serverCallback(req, res) {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end("Hello " + process.argv[2]);
+}
+
+http.createServer(serverCallback).listen(8090);
+
+//console.log("Hello World")
